b00cebdd62471cd05a636c6f324b71a479497aad
Peng <pengzhangzhang@gmail.com>
2018-04-28 08:39:24 +0300

    More updates in StreamExample

--- akka/src/main/scala/StreamExample.scala
+++ akka/src/main/scala/StreamExample.scala
@@ -1,16 +1,18 @@
 import java.nio.file.Paths
 
 import akka.NotUsed
-import akka.actor.{Actor, ActorRef, ActorSystem, Cancellable, Props}
+import akka.actor.{Actor, ActorSystem, Cancellable, Props}
+import akka.event.Logging
 import akka.stream._
+import akka.stream.scaladsl.Tcp.ServerBinding
 import akka.stream.scaladsl.{Flow, _}
 import akka.util.ByteString
 import com.typesafe.config.ConfigFactory
 
-import scala.collection.mutable
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Random
 
 object StreamExample extends App {
   val config = ConfigFactory.load()
@@ -193,10 +195,19 @@ object StreamExample extends App {
     val source = Source(1 to 10)
     source.buffer(1, OverflowStrategy.dropNew)
 
-    import scala.concurrent.duration._
     import akka.util.Timeout
 
+    import scala.concurrent.duration._
+
     implicit val duration: Timeout = 20 seconds
+
+    class DestActor extends Actor {
+      def receive = {
+        case i: Int =>
+          sender() ! i
+
+      }
+    }
 
     val dstRef = system.actorOf(Props(new DestActor), name = "dst")
 
@@ -206,17 +217,86 @@ object StreamExample extends App {
 
   }
 
-  testActor
+  def testLog = {
+    Source(-5 to 5)
+      .map(1 / _) //throwing ArithmeticException: / by zero
+      .log("error logging")
+      .runWith(Sink.ignore)
 
-
-  class DestActor extends Actor {
-    def receive = {
-      case i: Int =>
-        sender() ! i
-
-    }
   }
-  
+
+  def testLog2 = {
+    Source(-5 to 5)
+      .map(1 / _) //throwing ArithmeticException: / by zero
+      .log("logging")
+      .withAttributes(
+        Attributes.logLevels(
+          onElement = Logging.InfoLevel,
+          onFinish = Logging.InfoLevel,
+          onFailure = Logging.ErrorLevel // Logging.InfoLevel here will not log the exception? This seems a bug
+        )
+      ).runWith(Sink.ignore)
+  }
+
+  def testLogging = {
+    val source = Source(1 to 3)
+    source.map{ i =>
+      println(i)
+      i
+    }.runWith(Sink.ignore)
+
+  }
+
+  def testRecover = {
+    Source(0 to 6).map(n ⇒
+      if (n < 5) n.toString
+      else throw new RuntimeException("Boom!")
+    ).recover {
+      case _: RuntimeException ⇒ "stream truncated"
+    }.runForeach(println)
+  }
+
+  def testRecoverWithRetries = {
+    val planB = Source(List("five", "six", "seven", "eight"))
+
+    Source(0 to 6).map(n ⇒
+      if (n < 5) n.toString
+      else throw new RuntimeException("Boom!")
+    ).recoverWithRetries(attempts = 1, {
+      case _: RuntimeException ⇒ planB
+    }).runForeach(println)
+  }
+
+  def testBind = {
+    val binding: Future[ServerBinding] =
+      Tcp().bind("127.0.0.1", 8888).to(Sink.foreach(println)).run()
+
+  }
+
+  def testStreamOrder = {
+    val source = Source(1 to 3)
+
+    logThread("start")
+    // The stream is run in an actor with a different thread
+    source.map(i => {
+        logThread("stream map: " + i)
+        i
+      }
+    ).runForeach(i => println("stream run " + i))
+
+    // This may be printed before logs in stream because Stream is run in a different actor
+    Thread.sleep(Random.nextInt(100))
+    logThread("end")
+  }
+
+  def logThread(step: String): Unit = {
+    val timestamp: Long = System.currentTimeMillis / 1000
+    val threadId = Thread.currentThread().getId()
+    println(s"Step $step Current time $timestamp and thread ID $threadId ")
+  }
+
+  testStreamOrder
+
 
 }
 
